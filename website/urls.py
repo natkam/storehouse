de143dd5311300f44937dbaf4581ab809b96d470
natka <natka_m@o2.pl>
2017-12-18 18:22:43 +0100

    Create a (very basic) view&template shelves_list.
    Add an url, a base template and a shelves_list template extension.

--- website/urls.py
+++ website/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('shelves/', views.shelves_list, name='shelves_list')
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Shelf
 
 def index(request):
     return HttpResponse("Hello world, you're at the website index.")
+
+def shelves_list(request):
+    shelves = Shelf.objects.all()
+    return render(request, 'website/shelves_list.html', {'shelves': shelves})

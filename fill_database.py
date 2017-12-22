7d8e2da5e4a28cb574ecb8d22d58cc6c25301045
natka <natka_m@o2.pl>
2017-12-22 05:58:59 +0100

    Write a script for filling the DB with random objects, add fixtures.

---
+++ fill_database.py
@@ -0,0 +1,41 @@
+import django
+django.setup()
+
+from random import choice
+from website.models import Shelf, Transport, Load
+from website.models import LOAD_TYPES, MAX_TYPES_OF_LOAD_ON_SHELF, MAX_LOADS_ON_SHELF, MAX_NUMBER_OF_SHELVES, MAX_NUMBER_OF_TRANSPORTS
+
+def create_shelves():
+    for i in range(MAX_NUMBER_OF_SHELVES):
+        new_shelf = Shelf.objects.create(number=i, position=i)
+        new_shelf.save()
+
+def create_random_transports():
+    for i in range(MAX_NUMBER_OF_TRANSPORTS):
+        transport = Transport.objects.create(number=i, load_type=choice(LOAD_TYPES))
+        transport.save()
+
+def generate_random_loads_for_one_shelf():
+    types_of_load = []
+    for i in range(MAX_TYPES_OF_LOAD_ON_SHELF):
+        types_of_load.append(choice(LOAD_TYPES)[0])
+    loads_pattern = []
+    for i in range(MAX_LOADS_ON_SHELF):
+        loads_pattern.append(choice(types_of_load))
+    return loads_pattern
+
+def fill_one_shelf_randomly(shelf):
+    loads_pattern = generate_random_loads_for_one_shelf()
+    for load_type in loads_pattern:
+        new_load = Load.objects.create(load_type=load_type, shelf=shelf)
+        new_load.save()
+
+def fill_all_shelves():
+    shelves = Shelf.objects.all()
+    for shelf in shelves:
+        fill_one_shelf_randomly(shelf)
+
+
+create_shelves()
+fill_all_shelves()
+create_random_transports()

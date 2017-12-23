c1c744ce35f760b09353e56a3c2d1671247afb0e
natka <natka_m@o2.pl>
2017-12-23 01:34:13 +0100

    Fix setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-python manage.py runserver
+python manage.py runserver &>/dev/null &
 python manage.py migrate
 python manage.py loaddata website/fixtures/full_random.json
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'pass')" | ./manage.py shell
+

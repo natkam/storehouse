67b66d3b31e729cad20deeb5e7c7bc62c8b87ab8
natka <natka_m@o2.pl>
2017-12-23 08:04:16 +0100

    A naive and very messy version of sorting algorithm (not even quite finished).

--- sort_shelves.py
+++ sort_shelves.py
@@ -2,62 +2,66 @@
 import django
 django.setup()
 # ^ tmp!!!
+
 from website.models import *
+from transfer_loads import get_transports_order, shift_shelves, transfer_loads_to_one_transport
 
-def get_transports_order():
-    transports_order = []
-    transports_types = Transport.objects.values_list('load_type', flat=True).order_by('number')
-    # transport_types is a QuerySet - a list of strings: 'A', 'B', etc.
-    for type_letter in transports_types:
-        transports_order.append(type_letter)
-    return transports_order
+ALL_SHELVES = Shelf.objects.all().order_by('number')
 
-def transfer_one_load(load, transport):
-    load.shelf = None
-    load.transport = transport
-    load.save()
+def get_load_counts_on_one_shelf(shelf):
+    shelf_types = shelf.load_set.values_list('load_type', flat=True).order_by('load_type')
+    shelf_types_list = list(shelf_types)
+    distinct_shelf_types = set(shelf_types_list)
+    load_counts = {}
+    for load_type in distinct_shelf_types:
+        count = shelf_types_list.count(load_type)  # shelf_types - a QuerySet, not a list!
+        load_counts[load_type] = count
+    return load_counts
 
-def transfer_loads_from_shelf(shelf, transport):
-    loads_on_shelf = shelf.load_set.all()
-    transport_type_letter = transport.load_type
-    load_counter = transport.load_set.count()
-    for load in loads_on_shelf:
-        if load.load_type == transport_type_letter:
-            transfer_one_load(load, transport)
-            load_counter += 1
-            if load_counter >= MAX_LOADS_IN_TRANSPORT:
-                break
-    return load_counter
+def get_load_counts_on_all_shelves():
+    all_loads = {}
+    for shelf in ALL_SHELVES:
+        one_shelf_loads = get_load_counts_on_one_shelf(shelf)
+        all_loads[shelf.number] = one_shelf_loads
+    return all_loads
 
-def shift_shelves(last_shelf_position):
-    """ 'last_shelf_position' is the position of the shelf which stays in the front of the line. """
-    shelves = Shelf.objects.all()  # .order_by('position')
-    aside = []
-    for shelf in shelves:
-        if shelf.position < last_shelf_position:
-            aside.append(shelf)
-            shelf.position = None
-        else:
-            shelf.position -= last_shelf_position
-        shelf.save() # 
-    for old_position in range(last_shelf_position):
-        shelf = aside[old_position]
-        shelf.position = MAX_NUMBER_OF_SHELVES + old_position - last_shelf_position
-        shelf.save()
+def sort_shelves_with_given_load(tr_load_type, shelves_to_sort):
+    # print(shelves_to_sort)
+    sorted_shelves = []
+    for shelf in shelves_to_sort:
+        for load_type in shelves_to_sort[shelf]:
+            if load_type == tr_load_type:
+                sorted_shelves.append((shelf, shelves_to_sort[shelf][load_type]))
+    sorted_shelves.sort(key=lambda pair: pair[1], reverse=True)
+    print('function', sorted_shelves)
+    return sorted_shelves
 
-def transfer_loads_to_one_transport(transport):
-    load_counter = transport.load_set.count()
-    last_shelf_position = 0
-    shelves = Shelf.objects.all()
-    for shelf in shelves:
-        load_counter = transfer_loads_from_shelf(shelf, transport)
-        print(shelf.number, load_counter)
-        if load_counter >= MAX_LOADS_IN_TRANSPORT:
-            last_shelf_position = shelf.position
-            break
-    if last_shelf_position:
-        shift_shelves(last_shelf_position)
-
-tr = Transport.objects.get(number=3)
-if tr.load_set.count() < MAX_LOADS_IN_TRANSPORT:
-    transfer_loads_to_one_transport(tr)
+transports_order = get_transports_order()
+shelves_to_sort = get_load_counts_on_all_shelves()   # dict
+sorted_shelves_list = []
+# for transport_type in transports_order:
+for tr_type in transports_order:
+    print('tr_type', tr_type)
+    tmp_sorted_shelves = sort_shelves_with_given_load(tr_type, shelves_to_sort) # list of tuples
+    if not tmp_sorted_shelves:
+        continue
+    loads_in_transport = 0
+    i = 0
+    sorted_shelves_list.append(tmp_sorted_shelves.pop(0))
+    print(sorted_shelves_list, sorted_shelves_list[-1][0])
+    del shelves_to_sort[sorted_shelves_list[-1][0]]
+    loads_in_transport += sorted_shelves_list[-1][1]
+    if tmp_sorted_shelves[0][1] >= MAX_LOADS_IN_TRANSPORT:
+        loads_in_transport = MAX_LOADS_IN_TRANSPORT
+        # return # break?
+    else:
+        while loads_in_transport < MAX_LOADS_IN_TRANSPORT and i < len(tmp_sorted_shelves):
+            sorted_shelves_list.append(tmp_sorted_shelves.pop(i))
+            print('while', sorted_shelves_list, sorted_shelves_list[-1][0])
+            del shelves_to_sort[sorted_shelves_list[-1][0]]
+            loads_in_transport += sorted_shelves_list[-1][1]
+            i += 1
+    print('last line', shelves_to_sort, loads_in_transport)
+for shelf in shelves_to_sort:
+    sorted_shelves_list.append((shelf, 0))
+print('sorted_shelves_list:', sorted_shelves_list)
--- transfer_loads.py
+++ transfer_loads.py
@@ -2,17 +2,14 @@
 The functions here handle transfering loads from shelves to transports appropriately.
 """
 
-# import django
-# django.setup()
+import django
+django.setup()
 
 from website.models import *
 
 def get_transports_order():
-    # transports_order = []
     transports_types = Transport.objects.values_list('load_type', flat=True).order_by('number')
     # transport_types is a QuerySet - a list of strings: 'A', 'B', etc.
-    # for type_letter in transports_types:
-    #     transports_order.append(type_letter)
     transports_order = [type_letter for type_letter in transports_types]
     return transports_order
 
@@ -21,7 +18,7 @@ def transfer_one_load(load, transport):
     load.transport = transport
     load.save()
 
-def transfer_loads_from_shelf(shelf, transport):
+def transfer_loads_from_one_shelf(shelf, transport):
     loads_on_shelf = shelf.load_set.all()
     transport_type_letter = transport.load_type
     load_counter = transport.load_set.count()
@@ -54,8 +51,7 @@ def transfer_loads_to_one_transport(transport):
     last_shelf_position = 0
     shelves = Shelf.objects.all()
     for shelf in shelves:
-        load_counter = transfer_loads_from_shelf(shelf, transport)
-        # print(shelf.number, load_counter)
+        load_counter = transfer_loads_from_one_shelf(shelf, transport)
         if load_counter >= MAX_LOADS_IN_TRANSPORT:
             last_shelf_position = shelf.position
             break
@@ -63,6 +59,6 @@ def transfer_loads_to_one_transport(transport):
         shift_shelves(last_shelf_position)
 
 # TODO: put that piece of code in another function, or put the if inside the function...?
-# tr = Transport.objects.get(number=3)
+# tr = Transport.objects.get(number=2)
 # if tr.load_set.count() < MAX_LOADS_IN_TRANSPORT:
 #     transfer_loads_to_one_transport(tr)
